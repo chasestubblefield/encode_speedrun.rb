9d03cdc475bfb975ed16f2c19caaa86c3917bfb2
Chase Stubblefield <chasestubblefield@gmail.com>
2016-01-02 13:32:54 -0800

    Move options parsing out of class and add --no-exec option

--- encode_speedrun.rb
+++ encode_speedrun.rb
@@ -4,60 +4,13 @@ require 'optparse'
 
 class EncodeSpeedrun
 
-  POSSIBLE_OVERSCAN = %w{0 5 6.25}
-
-  def initialize
-    @input = nil
-    @output = nil
-    @start_frame = 0
-    @end_frame = nil
-    @overscan = 0
-    @sample_duration = nil
-    @mode = :encode
-
-    OptionParser.new do |opts|
-      opts.banner = 'Usage: encode_speedrun -i input.ts -o output.mp4 -s 300 -e 3000 [options]'
-
-      opts.on('-i', '--input FILE', 'Input recording') do |file|
-        @input = file
-      end
-      opts.on('-o', '--output FILE', 'Output MP4 file (ex. speedrun.mp4)') do |file|
-        @output = file
-      end
-      opts.on('-s', '--start FRAME', Integer, 'Starting frame of speedrun (default 0), output will start here') do |start_frame|
-        @start_frame = start_frame
-      end
-      opts.on('-e', '--end FRAME', Integer, 'Final frame of speedrun, timecode will stop here') do |end_frame|
-        @end_frame = end_frame
-      end
-      opts.on('--overscan PERCENT', POSSIBLE_OVERSCAN, "Percentage to overscan, (default 0). Possible values: #{POSSIBLE_OVERSCAN.join(', ')}") do |percent|
-        @overscan = percent.to_f / 100
-      end
-      opts.on('--sample DURATION', Integer, 'Limit output to DURATION seconds, useful for testing') do |duration|
-        @sample_duration = duration
-      end
-      opts.on('--frames', 'Output a 1 minute video with frame counts to determine start and finish') do
-        @mode = :frames
-      end
-      opts.on_tail("-h", "--help", "Show this message") do
-        puts opts
-        exit
-      end
-    end.parse!
-
-    unless @input && @output && @end_frame
-      abort %q{One of input, ouput, or end was not specified. Please use "encode_speedrun -h" to see options.}
-    end
-  end
-
-  def ffmpeg_cmd
-    @ffmpeg_cmd ||= begin
-      if @mode == :encode
-        ffmpeg_encode_cmd
-      elsif @mode == :frames
-        ffmpeg_frames_cmd
-      end
-    end
+  def initialize(input, output, start, finish, options={})
+    @input = input
+    @output = output
+    @start = start || 0
+    @finish = finish
+    @overscan = options[:overscan] || 0
+    @sample = options[:sample] || nil
   end
 
   def ffmpeg_encode_cmd
@@ -67,7 +20,7 @@ class EncodeSpeedrun
 
     vfilters = []
     vfilters.push 'fps=30'
-    vfilters.push "trim=start_frame=#{@start_frame}"
+    vfilters.push "trim=start_frame=#{@start}"
     vfilters.push 'setpts=PTS-STARTPTS'
     vfilters.push 'crop=704:480,scale=640:480'
     if @overscan > 0
@@ -109,9 +62,9 @@ class EncodeSpeedrun
   end
 
   def ffmpeg_frames_cmd
-    start_seconds = @start_frame
+    start_seconds = @start
     start_seconds -= 15 if start_seconds > 15
-    end_seconds = @end_frame
+    end_seconds = @finish
     end_seconds -= 15 if end_seconds > 15
 
     cmd = ['ffmpeg']
@@ -135,11 +88,11 @@ class EncodeSpeedrun
   end
 
   def total_frames
-    @total_frames ||= @end_frame - @start_frame
+    @total_frames ||= @finish - @start
   end
 
   def start_sample
-    @start_sample ||= @start_frame * 1600
+    @start_sample ||= @start * 1600
   end
 
   def final_timecode
@@ -169,7 +122,56 @@ class EncodeSpeedrun
 end
 
 if __FILE__ == $0
-  cmd = EncodeSpeedrun.new.ffmpeg_cmd
+  options = {}
+  required_options = %i(input output finish)
+  possible_overscan = %w{0 5 6.25}
+
+  OptionParser.new do |opts|
+    opts.banner = 'Usage: encode_speedrun -i input.ts -o output.mp4 -s 300 -e 3000 [options]'
+
+    opts.on('-i', '--input FILE', 'Input recording') do |file|
+      options[:input] = file
+    end
+    opts.on('-o', '--output FILE', 'Output MP4 file (ex. speedrun.mp4)') do |file|
+      options[:output] = file
+    end
+    opts.on('-s', '--start N', Integer, 'Starting frame of speedrun (default 0), output will start here') do |n|
+      options[:start] = n
+    end
+    opts.on('-f', '--finish N', Integer, 'Final frame of speedrun, timecode will stop here') do |n|
+      options[:finish] = n
+    end
+    opts.on('--overscan PERCENT', possible_overscan, "Percentage to overscan, (default 0). Possible values: #{possible_overscan.join(', ')}") do |percent|
+      options[:overscan] = percent.to_f / 100
+    end
+    opts.on('--sample DURATION', Integer, 'Limit output to DURATION seconds, useful for testing') do |duration|
+      options[:sample] = duration
+    end
+    opts.on('--frames', 'Output a 1 minute video with frame counts to determine exact start and finish') do
+      options[:frames_only] = true
+    end
+    opts.on('--[no-]exec', 'Execute the ffmpeg command, or just print it') do |ex|
+      options[:exec] = ex
+    end
+    opts.on_tail("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  required_options.each do |key|
+    unless options.has_key?(key)
+      abort %Q{Required option #{key} not specified. Please use "encode_speedrun -h" to see options.}
+    end
+  end
+
+  e = EncodeSpeedrun.new(options[:input], options[:output], options[:start], options[:finish],
+    :overscan => options[:overscan], :sample =>  options[:sample])
+  if options[:frames_only]
+    cmd = e.ffmpeg_frames_cmd
+  else
+    cmd = e.ffmpeg_encode_cmd
+  end
   puts cmd
-  exec cmd
+  exec cmd if options[:exec]
 end
